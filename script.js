3ecacd5fc88653da81dbfd5384bcc10cbbd5637f
abohannon <abo46n2@gmail.com>
2017-07-16 16:25:36 -0700

    Changed HTML structure, more js logic work

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var htmlGrid = document.querySelector("#grid");
-var rows = document.querySelectorAll("tr");
-var cells = document.querySelectorAll("td");
+// select all our elements
+var htmlGrid = document.querySelector(".grid");
+var cells = document.querySelectorAll(".cell");
+var playerSelect = document.querySelector(".welcome");
 var reset = document.querySelector("#reset");
+
+// app variables
+var player = null;
+var computer = null;
+var playerTurn = false;
 
 var myMove = false;
 var gridArray = [];
+
 
 var board = [
 
@@ -14,29 +21,86 @@ var board = [
 
 ];
 
-// turn the table cells into an array
-for (var i = 0; i < rows.length; i++){
-  console.log(rows[i]);
-  gridArray.push(rows[i].querySelectorAll("td"));
+// choose to play as X or O
+playerSelect.addEventListener("click", choosePlayer);
+
+function choosePlayer(e) {
+  if (e.target !== e.currentTarget) {
+
+    if (e.target.innerHTML === "X") {
+      player = "X";
+      computer = "O";
+
+    }
+    if (e.target.innerHTML === "O") {
+      player = "O";
+      computer = "X";
+
+    }
+
+  }
 }
 
 // listen for clicks on the board
 htmlGrid.addEventListener("click", gameInit);
 
-function gameInit(e){
-  if (e.target !== e.currentTarget){
+function gameInit(e) {
+  if (e.target !== e.currentTarget) {
+    console.log(e.target);
 
-    if (e.target === c00) {
-      board[0][0] = 1;
-      e.target.innerHTML = "X";
+    e.target.innerHTML = player;
+
+    if (e.target === c00){
+      board[0][0] = player;
+    }
+    if (e.target === c01){
+      board[0][1] = player;
+    }
+    if (e.target === c02){
+      board[0][2] = player;
+    }
+    if (e.target === c10){
+      board[1][0] = player;
+    }
+    if (e.target === c11){
+      board[1][1] = player;
+    }
+    if (e.target === c12){
+      board[1][2] = player;
+    }
+    if (e.target === c20){
+      board[2][0] = player;
+    }
+    if (e.target === c21){
+      board[2][1] = player;
+    }
+    if (e.target === c22){
+      board[2][2] = player;
     }
 
 
-
-
+    declareWinner();
 
   }
 }
+
+// declare the winner
+function declareWinner() {
+
+    // horizontal
+    if (board[0].includes(null) === false && board[0][0] === board[0][1] && board[0][1] === board[0][2]){
+      console.log("winner");
+    }
+
+    // vertical
+
+    // diagonal
+
+
+}
+
+
+
 
 
 
@@ -52,7 +116,7 @@ function restartGame() {
   ];
 
   // loop through all cells and clear moves
-  for (var i = 0; i < cells.length; i++){
+  for (var i = 0; i < cells.length; i++) {
     cells[i].innerHTML = "";
   }
 
