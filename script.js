8a001801e2d8edd073402607fb7cbf9282985602
abohannon <abo46n2@gmail.com>
2017-07-19 18:46:30 -0700

    Completed winner logic

--- script.js
+++ script.js
@@ -91,20 +91,33 @@ function declareWinner() {
     if ((boardArray[0] !== "" && boardArray[0] === boardArray[1] && boardArray[1] === boardArray[2]) ||
       (boardArray[3] !== "" && boardArray[3] === boardArray[4] && boardArray[4] === boardArray[5]) ||
       (boardArray[6] !== "" && boardArray[6] === boardArray[7] && boardArray[7] === boardArray[8])) {
-      if (turn === "X") {
-        announceWinner.innerHTML = "<h2>O is the winner!<h2>";
-      } else {
-        announceWinner.innerHTML = "<h2>X is the winner!<h2>";
-      }
+        if (turn === "X") {
+          announceWinner.innerHTML = "<h2>O is the winner!<h2>";
+        } else {
+          announceWinner.innerHTML = "<h2>X is the winner!<h2>";
+        }
     }
 
     // vertical
-
-
-
-
+    if ((boardArray[0] !== "" && boardArray[0] === boardArray[3] && boardArray[3] === boardArray[6]) ||
+      (boardArray[1] !== "" && boardArray[1] === boardArray[4] && boardArray[4] === boardArray[7]) ||
+      (boardArray[2] !== "" && boardArray[2] === boardArray[5] && boardArray[5] === boardArray[8])) {
+        if (turn === "X") {
+          announceWinner.innerHTML = "<h2>O is the winner!<h2>";
+        } else {
+          announceWinner.innerHTML = "<h2>X is the winner!<h2>";
+        }
+    }
 
     // diagonal
+    if ((boardArray[0] !== "" && boardArray[0] === boardArray[4] && boardArray[4] === boardArray[8]) ||
+      (boardArray[2] !== "" && boardArray[2] === boardArray[4] && boardArray[4] === boardArray[6])) {
+        if (turn === "X") {
+          announceWinner.innerHTML = "<h2>O is the winner!<h2>";
+        } else {
+          announceWinner.innerHTML = "<h2>X is the winner!<h2>";
+        }
+    }
 
   } else {
     announceWinner.innerHTML = "<h2>It's a draw.<h2>";
