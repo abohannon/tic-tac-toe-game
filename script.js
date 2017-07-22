e785964082869175f2299df053fc674959499060
abohannon <abo46n2@gmail.com>
2017-07-21 22:45:42 -0700

    Prevented replacing of moves

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// TODO: Setup AI
+// TODO: Reset board on win
+
 var grid = document.querySelector('.grid');
 var cells = document.querySelectorAll('.cell');
 var playerSelect = document.querySelector('.welcome');
@@ -5,15 +8,16 @@ var scoreboard = document.querySelector('.scoreboard');
 var reset = document.querySelector('#reset');
 var winnerMsg = document.querySelector('#winner-msg');
 
-var turn = "X";
+var turn = "";
 var player;
 var computer;
+var availSpots;
 
 var board = [
 
-  0,1,2,
-  3,4,5,
-  6,7,8
+  0, 1, 2,
+  3, 4, 5,
+  6, 7, 8
 
 ];
 
@@ -38,23 +42,25 @@ function choosePlayer(e) {
 // game board functionality
 grid.addEventListener('click', updateBoard, false);
 
-function updateBoard(e){
-  if (e.target.className == "cell"){
+function updateBoard(e) {
+  if (e.target.className == "cell") {
+    if (e.target.innerHTML !== "O" && e.target.innerHTML !== "X"){
+
 
     e.target.innerHTML = turn;
 
-    for (var i = 0; i < board.length; i++){
-      if (cells[i].innerHTML !== ""){
+    for (var i = 0; i < board.length; i++) {
+      if (cells[i].innerHTML !== "") {
         board[i] = cells[i].innerHTML;
       }
     }
 
     if (declareWinner(board, player)) {
-      console.log("Player wins");
+      winnerMsg.innerHTML = "<h2>Player wins!</h2>";
     } else if (declareWinner(board, computer)) {
-      console.log("Computer wins");
+      winnerMsg.innerHTML = "<h2>Computer wins!</h2>";
     }
-
+    }
   }
 }
 
@@ -65,79 +71,43 @@ function emptySpots(board) {
   });
 }
 
-// tha main minimax function
-// function minimax(newBoard, player) {
-//
-//   // available spots
-//   var availSpots = emptySpots(newBoard);
-//
-//   if (winning(newBoard, player)){
-//     return {score:-10};
-//   }
-//
-//     else if (winning(newBoard, computer)){
-//       return {score:10};
-//     }
-//
-//     else if (availSpots.length === 0) {
-//       return {score:0};
-//     }
-//
-//     var moves = [];
-//
-//     for (var i = 0; i < availSpots.length; i++) {
-//       var move = {};
-//           move.index = newBoard[availSpots[i]];
-//
-//       newBoard[availSpots[i]] = player;
-//
-//       if (player == computer) {
-//
-//       }
-//     }
-//
-// }
-
-
-
-
 
 // declare winner
-function declareWinner(gameBoard, move){
+function declareWinner(gameBoard, move) {
 
   if (
-   (gameBoard[0] == move && gameBoard[1] == move && gameBoard[2] == move) ||
-   (gameBoard[3] == move && gameBoard[4] == move && gameBoard[5] == move) ||
-   (gameBoard[6] == move && gameBoard[7] == move && gameBoard[8] == move) ||
-   (gameBoard[0] == move && gameBoard[3] == move && gameBoard[6] == move) ||
-   (gameBoard[1] == move && gameBoard[4] == move && gameBoard[7] == move) ||
-   (gameBoard[2] == move && gameBoard[5] == move && gameBoard[8] == move) ||
-   (gameBoard[0] == move && gameBoard[4] == move && gameBoard[8] == move) ||
-   (gameBoard[2] == move && gameBoard[4] == move && gameBoard[6] == move)
-   ) {
-   return true;
-   } else {
-   return false;
-   }
+    (gameBoard[0] == move && gameBoard[1] == move && gameBoard[2] == move) ||
+    (gameBoard[3] == move && gameBoard[4] == move && gameBoard[5] == move) ||
+    (gameBoard[6] == move && gameBoard[7] == move && gameBoard[8] == move) ||
+    (gameBoard[0] == move && gameBoard[3] == move && gameBoard[6] == move) ||
+    (gameBoard[1] == move && gameBoard[4] == move && gameBoard[7] == move) ||
+    (gameBoard[2] == move && gameBoard[5] == move && gameBoard[8] == move) ||
+    (gameBoard[0] == move && gameBoard[4] == move && gameBoard[8] == move) ||
+    (gameBoard[2] == move && gameBoard[4] == move && gameBoard[6] == move)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 
 }
 
-
-
 // reset game
-reset.addEventListener('click', function(){
+reset.addEventListener('click', function() {
 
   console.clear();
 
+  turn = "";
+
   board = [
 
-    0,1,2,
-    3,4,5,
-    6,7,8
+    0, 1, 2,
+    3, 4, 5,
+    6, 7, 8
 
   ];
 
-  cells.forEach(function(cell){
+  cells.forEach(function(cell) {
     cell.innerHTML = "";
   });
 
