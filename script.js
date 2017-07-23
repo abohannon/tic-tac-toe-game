32316186af8af2e0ebf56d663c7bced408a38300
abohannon <abo46n2@gmail.com>
2017-07-23 11:51:10 -0700

    Worked on computer AI

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // TODO: Setup AI
 // TODO: Reset board on win
+// TODO: Declare draw
 
 var grid = document.querySelector('.grid');
 var cells = document.querySelectorAll('.cell');
@@ -12,6 +13,7 @@ var turn = "";
 var player;
 var computer;
 var availSpots;
+var moves = 0;
 
 var board = [
 
@@ -19,6 +21,17 @@ var board = [
   3, 4, 5,
   6, 7, 8
 
+];
+
+var wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
 
 // choose player
@@ -44,24 +57,32 @@ grid.addEventListener('click', updateBoard, false);
 
 function updateBoard(e) {
   if (e.target.className == "cell") {
-    if (e.target.innerHTML !== "O" && e.target.innerHTML !== "X"){
+    if (e.target.innerHTML !== "O" && e.target.innerHTML !== "X") {
 
 
-    e.target.innerHTML = turn;
+      e.target.innerHTML = turn;
 
-    for (var i = 0; i < board.length; i++) {
-      if (cells[i].innerHTML !== "") {
-        board[i] = cells[i].innerHTML;
+      for (var i = 0; i < board.length; i++) {
+        if (cells[i].innerHTML !== "") {
+          board[i] = cells[i].innerHTML;
+        }
       }
-    }
 
-    if (declareWinner(board, player)) {
-      winnerMsg.innerHTML = "<h2>Player wins!</h2>";
-    } else if (declareWinner(board, computer)) {
-      winnerMsg.innerHTML = "<h2>Computer wins!</h2>";
+      computerMove();
+
+      console.log("empty spots:", emptySpots(board));
+
+      // call declareWinner function and determine who wins
+      // TODO: implement draw if moves are 9 with no winner
+
+        if (declareWinner(board, player)) {
+          winnerMsg.innerHTML = "<h2>Player wins!</h2>";
+        } else if (declareWinner(board, computer)) {
+          winnerMsg.innerHTML = "<h2>Computer wins!</h2>";
+        }
+      }
+
     }
-    }
-  }
 }
 
 // return list of empty board spots
@@ -71,8 +92,163 @@ function emptySpots(board) {
   });
 }
 
+function computerMove() {
 
-// declare winner
+  // if the computer can take the center, take it
+  // if (emptySpots(board).includes(4)) {
+  //   setTimeout(function() {
+  //     board[4] = cells[4].innerHTML = computer;
+  //   }, 1000);
+  // } else if (emptySpots(board).includes(3) && board[6] === player) {
+  //   setTimeout(function() {
+  //     board[3] = cells[3].innerHTML = computer;
+  //   }, 1000);
+  // }
+
+  if (emptySpots(board).includes(4)) {
+    setTimeout(function() {
+      board[4] = cells[4].innerHTML = computer;
+    }, 1000);
+  }
+  else if ( board[0] === player &&
+            board[1] === player &&
+            board[2] === 2) {
+    setTimeout(function() {board[2] = cells[2].innerHTML = computer;},1000);
+  }
+  else if ( board[1] === player &&
+            board[2] === player &&
+            board[0] === 0) {
+    setTimeout(function() {board[0] = cells[0].innerHTML = computer;},1000);
+  }
+  else if ( board[0] === player &&
+            board[2] === player &&
+            board[1] === 1) {
+    board[1] = cells[1].innerHTML = computer;
+  } // top horizontal
+  else if ( board[3] === player &&
+            board[4] === player &&
+            board[5] === 5) {
+    board[5] = cells[5].innerHTML = computer;
+  }
+  else if ( board[4] === player &&
+            board[5] === player &&
+            board[3] === 3) {
+    board[3] = cells[3].innerHTML = computer;
+  }
+  else if ( board[3] === player &&
+            board[5] === player &&
+            board[4] === 4) {
+    board[4] = cells[4].innerHTML = computer;
+  } // middle horizontal
+  else if ( board[6] === player &&
+            board[7] === player &&
+            board[8] === 8) {
+    board[8] = cells[8].innerHTML = computer;
+  }
+  else if ( board[7] === player &&
+            board[8] === player &&
+            board[6] === 6) {
+    board[6] = cells[6].innerHTML = computer;
+  }
+  else if ( board[6] === player &&
+            board[8] === player &&
+            board[7] === 7) {
+    board[7] = cells[7].innerHTML = computer;
+  } // bottom horizontal
+  else if ( board[0] === player &&
+            board[3] === player &&
+            board[6] === 6) {
+    board[6] = cells[6].innerHTML = computer;
+  }
+  else if ( board[3] === player &&
+            board[6] === player &&
+            board[0] === 0) {
+    board[0] = cells[0].innerHTML = computer;
+  }
+  else if ( board[0] === player &&
+            board[6] === player &&
+            board[3] === 3) {
+    board[3] = cells[3].innerHTML = computer;
+  } // left vertical
+  else if ( board[1] === player &&
+            board[4] === player &&
+            board[7] === 7) {
+    board[7] = cells[7].innerHTML = computer;
+  }
+  else if ( board[4] === player &&
+            board[7] === player &&
+            board[1] === 1) {
+    board[1] = cells[1].innerHTML = computer;
+  }
+  else if ( board[1] === player &&
+            board[7] === player &&
+            board[4] === 4) {
+    board[4] = cells[4].innerHTML = computer;
+  } // middle vertical
+  else if ( board[2] === player &&
+            board[5] === player &&
+            board[8] === 8) {
+    board[8] = cells[8].innerHTML = computer;
+  }
+  else if ( board[5] === player &&
+            board[8] === player &&
+            board[2] === 2) {
+    board[2] = cells[2].innerHTML = computer;
+  }
+  else if ( board[2] === player &&
+            board[8] === player &&
+            board[5] === 5) {
+    board[5] = cells[5].innerHTML = computer;
+  } // right vertical
+  else if ( board[0] === player &&
+            board[4] === player &&
+            board[8] === 8) {
+    board[8] = cells[8].innerHTML = computer;
+  }
+  else if ( board[4] === player &&
+            board[8] === player &&
+            board[0] === 0) {
+    setTimeout(function(){board[0] = cells[0].innerHTML = computer;},1000);
+  }
+  else if ( board[0] === player &&
+            board[8] === player &&
+            board[4] === 4) {
+    setTimeout(function(){board[4] = cells[4].innerHTML = computer;},1000);
+  } // left diagonal
+  else if ( board[2] === player &&
+            board[4] === player &&
+            board[6] === 6) {
+    setTimeout(function(){board[6] = cells[6].innerHTML = computer;},1000);
+  }
+  else if ( board[4] === player &&
+            board[6] === player &&
+            board[2] === 2) {
+    setTimeout(function(){board[2] = cells[2].innerHTML = computer;},1000);
+  }
+  else if ( board[2] === player &&
+            board[6] === player &&
+            board[4] === 4) {
+    setTimeout(function(){board[4] = cells[4].innerHTML = computer;},1000);
+  } // right diagonal
+  else {
+      computerRandom();
+  }
+
+}
+
+// TODO: this sort of works, need to perfect.
+function computerRandom(){
+
+  for (var i = 0; i < emptySpots(board).length; i++){
+    var rand = Math.floor(Math.random() * i);
+    board[i] = cells[i].innerHTML = computer;
+    console.log(emptySpots(board)[rand]);
+    break;
+  }
+
+}
+
+// function to pick winner
 function declareWinner(gameBoard, move) {
 
   if (
@@ -98,6 +274,8 @@ reset.addEventListener('click', function() {
   console.clear();
 
   turn = "";
+
+  moves = 0;
 
   board = [
 
